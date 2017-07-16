7120e585888fb6a3d22456442daadd969b16a8c4
Sing Mak <mssjsg@gmail.com>
2017-07-16 22:41:56 +0800

    Add inject annotation to logger.

--- base/src/main/java/io/github/mssjsg/android/base/util/logger/LogCatLogger.java
+++ base/src/main/java/io/github/mssjsg/android/base/util/logger/LogCatLogger.java
@@ -2,13 +2,20 @@ package io.github.mssjsg.android.base.util.logger;
 
 import android.util.Log;
 
+import javax.inject.Inject;
+import javax.inject.Singleton;
+
 /**
  * Created by Sing on 26/3/2017.
  */
-
+@Singleton
 public class LogCatLogger extends Logger {
 
     private static final String PREFIX = "[BookBag]";
+
+    @Inject
+    public LogCatLogger() {
+    }
 
     @Override
     protected void debug(String tag, String message) {
