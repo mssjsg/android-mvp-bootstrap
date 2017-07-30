d58b41fe35cb204d2d2433336e42e4418b2b8e99
Sing Mak <mssjsg@gmail.com>
2017-07-30 17:13:00 +0800

    Refactor interactor.

--- base/src/main/java/io/github/mssjsg/android/base/usecase/Interactor.java
+++ base/src/main/java/io/github/mssjsg/android/base/usecase/Interactor.java
@@ -2,7 +2,9 @@ package io.github.mssjsg.android.base.usecase;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.Callable;
+import java.util.concurrent.CopyOnWriteArraySet;
 import java.util.concurrent.Executor;
 import java.util.concurrent.Future;
 import java.util.concurrent.FutureTask;
@@ -16,9 +18,7 @@ public abstract class Interactor<Param, Progress, Result> {
     private Executor mBackgroundExecutor;
     private Executor mPostExecuteExecutor;
 
-    private volatile Callback<Param, Progress, Result> mCallback;
-
-    private final Map<Object, Future<Result>> mFutures = new HashMap<>();
+    private final Map<Object, TaskItem<Param, Progress, Result>> mFutures = new HashMap<>();
 
     private static final Object NULL_PARAM = new Object();
 
@@ -27,51 +27,45 @@ public abstract class Interactor<Param, Progress, Result> {
         mPostExecuteExecutor = postExecuteExecutor;
     }
 
-    public void setCallback(Callback<Param, Progress, Result> callback) {
-        mCallback = callback;
-    }
-
-    private void post(final Runnable runnable) {
-        if (mCallback != null) {
-            mPostExecuteExecutor.execute(new Runnable() {
-                @Override
-                public void run() {
-                    if (mCallback != null) {
-                        runnable.run();
-                    }
-                }
-            });
-        }
-    }
-
     protected void postProgress(final Param param, final Progress progress) {
-        post(new Runnable() {
+        final TaskItem<Param, Progress, Result> taskItem = getTaskItem(param);
+        mPostExecuteExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                mCallback.onProgress(param, progress);
+                taskItem.mCallbacks.onProgress(param, progress);
             }
         });
     }
 
     protected void postResult(final Param param, final Result result) {
-        post(new Runnable() {
+        final TaskItem<Param, Progress, Result> taskItem = getTaskItem(param);
+        mPostExecuteExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                mCallback.onSuccess(param, result);
+                taskItem.mCallbacks.onSuccess(param, result);
             }
         });
     }
 
     protected void postException(final Param param, final Exception exception) {
-        post(new Runnable() {
+        final TaskItem<Param, Progress, Result> taskItem = getTaskItem(param);
+        mPostExecuteExecutor.execute(new Runnable() {
             @Override
             public void run() {
-                mCallback.onFailure(param, exception);
+                taskItem.mCallbacks.onFailure(param, exception);
             }
         });
     }
 
-    public final Future<Result> execute(final Param param) {
+    private TaskItem<Param, Progress, Result> getTaskItem(Param param) {
+        if (param == null) {
+            return mFutures.get(NULL_PARAM);
+        } else {
+            return mFutures.get(param);
+        }
+    }
+
+    public final Future<Result> execute(final Param param, final Callback<Param, Progress, Result> callback) {
 
         final Object futureKey;
         if (param == null) {
@@ -81,13 +75,17 @@ public abstract class Interactor<Param, Progress, Result> {
         }
 
         synchronized (mFutures) {
-            if (mFutures.get(futureKey) == null) {
+            TaskItem<Param, Progress, Result> taskItem = mFutures.get(futureKey);
+
+            if (taskItem == null) {
                 //only submit a new task when it's not in progress
 
                 Callable<Result> callable = new Callable<Result>() {
                     @Override
                     public Result call() throws Exception {
-                        boolean taskCancelled = mFutures.get(futureKey).isCancelled();
+                        TaskItem<Param, Progress, Result> taskItem = mFutures.get(futureKey);
+
+                        boolean taskCancelled = taskItem.mResultFuture.isCancelled();
                         try {
                             Result result = onExecute(param);
                             if (!taskCancelled) {
@@ -106,13 +104,14 @@ public abstract class Interactor<Param, Progress, Result> {
                 };
 
                 FutureTask<Result> futureTask = new FutureTask<>(callable);
-
-                mFutures.put(futureKey, futureTask);
+                mFutures.put(futureKey, new TaskItem<>(futureTask, callback));
 
                 mBackgroundExecutor.execute(futureTask);
+            } else {
+                mFutures.get(futureKey).mCallbacks.addCallback(callback);
             }
 
-            return mFutures.get(futureKey);
+            return mFutures.get(futureKey).mResultFuture;
         }
     }
 
@@ -124,5 +123,54 @@ public abstract class Interactor<Param, Progress, Result> {
         void onSuccess(Param param, Result result);
 
         void onFailure(Param param, Exception exception);
+    }
+
+    private static class TaskItem<Param, Progress, Result> {
+        Future<Result> mResultFuture;
+        CallbackWrapper<Param, Progress, Result> mCallbacks;
+
+        TaskItem(Future<Result> resultFuture, Callback<Param, Progress, Result> callback) {
+            mResultFuture = resultFuture;
+            mCallbacks = new CallbackWrapper<>(callback);
+        }
+    }
+
+    private static class CallbackWrapper<Param, Progress, Result> implements Callback<Param, Progress, Result> {
+
+        Set<Callback<Param, Progress, Result>> mCallbacks;
+
+        public CallbackWrapper(Callback<Param, Progress, Result> callback) {
+            mCallbacks = new CopyOnWriteArraySet<>();
+            if (callback != null) {
+                mCallbacks.add(callback);
+            }
+        }
+
+        @Override
+        public void onProgress(Param param, Progress progress) {
+            for (Callback<Param, Progress, Result> callback : mCallbacks) {
+                callback.onProgress(param, progress);
+            }
+        }
+
+        @Override
+        public void onSuccess(Param param, Result result) {
+            for (Callback<Param, Progress, Result> callback : mCallbacks) {
+                callback.onSuccess(param, result);
+            }
+        }
+
+        @Override
+        public void onFailure(Param param, Exception exception) {
+            for (Callback<Param, Progress, Result> callback : mCallbacks) {
+                callback.onFailure(param, exception);
+            }
+        }
+
+        void addCallback(Callback<Param, Progress, Result> callback) {
+            if (callback != null) {
+                mCallbacks.add(callback);
+            }
+        }
     }
 }
